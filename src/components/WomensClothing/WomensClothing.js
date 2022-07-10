5e210e7d073ce7e5fab4bc0fc984677fbf346b34
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-07-10 20:40:04 +0200

    Refactor code to extract ProductItem component and reduce code for Electronics, Mens Clothing, Womens Clothing and Jewelery components

--- src/components/WomensClothing/WomensClothing.js
+++ src/components/WomensClothing/WomensClothing.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { addToCart } from "../../services/addToCart";
+import ProductItem from "../ProductItem/ProductItem";
 import "./WomensClothing.css";
 
 const WomensClothing = ({ addedNotificationTimer, productsToShow }) => {
@@ -20,39 +19,10 @@ const WomensClothing = ({ addedNotificationTimer, productsToShow }) => {
     <div className="products">
       {womensClothing.map((product) => {
         return (
-          <div className="singleProduct" key={product.id}>
-            <Link to={`/single-product/${product.id}`}>
-              <div className="imageDiv">
-                <img
-                  className="productImg"
-                  src={product.image}
-                  alt={product.title}
-                ></img>
-              </div>
-
-              <p className="productTitle">{product.title}</p>
-            </Link>
-            <input
-              type="number"
-              defaultValue="1"
-              onChange={(event) => (product.amount = () => event.target.value)}
-              min="1"
-              max="10"
-            ></input>
-
-            <p>Price: ${(product.price * product.amount).toFixed(2)}</p>
-            <p>Rating: {product.rating.rate}</p>
-
-            <button
-              className="addToCartButton"
-              onClick={() => {
-                addToCart(product, product.amount, womensClothing);
-                addedNotificationTimer();
-              }}
-            >
-              Add to cart
-            </button>
-          </div>
+          <ProductItem
+            product={product}
+            addedNotificationTimer={addedNotificationTimer}
+          />
         );
       })}
     </div>
